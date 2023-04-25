bb0b7b2991f7f909b8537ff72de7c7f2cb22c6dd
Bruno Rodrigues <bruno.brd73@gmail.com>
2023-04-25 15:24:53 -0300

    feat: if applied, this commit will add unidade curricular's service

---
+++ src/main/java/br/com/aprendizagem/api/service/ModuloService.java
@@ -0,0 +1,23 @@
+package br.com.aprendizagem.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import br.com.aprendizagem.api.entity.Modulo;
+import br.com.aprendizagem.api.entity.UnidadeCurricular;
+import br.com.aprendizagem.api.repository.ModuloRepository;
+import br.com.aprendizagem.api.repository.UnidadeCurricularRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ModuloService {
+    
+    @Autowired
+    private UnidadeCurricularRepository unidadeCurricularRepository; 
+
+}
