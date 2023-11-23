7fb3141b237b07dfce89078498a5652dfa8de5d0
= <lipesanmartin@hotmail.com>
2023-11-23 15:53:17 -0300

    Post de comentario funcionando pelo front

--- src/main/java/br/com/aprendizagem/api/DTO/ChapterAssuntoComentarioDto.java
+++ src/main/java/br/com/aprendizagem/api/DTO/ChapterAssuntoComentarioDto.java
@@ -1,18 +1,13 @@
 package br.com.aprendizagem.api.DTO;
 
-import br.com.aprendizagem.api.entity.ChapterAssunto;
-import br.com.aprendizagem.api.entity.ChapterAssuntoComentario;
 import br.com.aprendizagem.api.entity.Curtida;
 import br.com.aprendizagem.api.entity.Usuario;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Set;
 
 @Getter
